ed240012d04511574aeb838ad9ef6a835c32767a
hejie <hejie@onemusicdream.com>
2017-09-03 17:13:11 +0800

    update

--- app/src/main/java/com/omd/AtlasLaunch.java
+++ app/src/main/java/com/omd/AtlasLaunch.java
@@ -3,9 +3,6 @@ package com.omd;
 import android.content.Context;
 import android.taobao.atlas.runtime.AtlasPreLauncher;
 
-/**
- * Created by WIN7 on 2017/6/1.
- */
 
 public class AtlasLaunch implements AtlasPreLauncher {
     @Override
