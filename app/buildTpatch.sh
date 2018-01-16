f0b7070ee7d04b2b7f9036d7020b3cdc8e3b9831
hejie <hejie@onemusicdream.com>
2018-01-16 16:56:38 +0800

    update

--- app/buildTpatch.sh
+++ app/buildTpatch.sh
@@ -4,11 +4,9 @@ echo "(1) 修改依赖的源码和版本"
 echo "    修改App工程源码"
 echo "    修改versionName"
 
-
 echo "(2)  重新构建apk， 生成patch 包  --info --debug"
 
 ../gradlew clean assembleDebug -DapVersion=1.0.0 -DversionName=1.0.1 --stacktrace
-
 
 echo "(3) 上传 patch"
 adb push ./build/outputs/tpatch-devDebug/update-*.json /sdcard/Android/data/com.omd/cache/update.json
